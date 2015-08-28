cf02ec2fb6f8aebae02d736c9efc4880f9b219ac
diegoheusser <diego.heusser@outlook.com>
2015-08-28 18:02:26 -0300

    Criado no projeto android e webservice

---
+++ web-service/CustoVidaWebService/src/java/br/udesc/ceavi/custovida/model/Control.java
@@ -0,0 +1,52 @@
+package br.udesc.ceavi.custodevida.model;
+
+import java.util.Date;
+
+public class Control {
+
+    private int id;
+    private Date emissionDate;
+    private int percentage;
+    private Source source;
+    private Researcher researcher;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getEmissionDate() {
+        return emissionDate;
+    }
+
+    public void setEmissionDate(Date emissionDate) {
+        this.emissionDate = emissionDate;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(int percentage) {
+        this.percentage = percentage;
+    }
+
+    public Source getSource() {
+        return source;
+    }
+
+    public void setSource(Source source) {
+        this.source = source;
+    }
+
+    public Researcher getResearcher() {
+        return researcher;
+    }
+
+    public void setResearcher(Researcher researcher) {
+        this.researcher = researcher;
+    }
+}
---
+++ web-service/CustoVidaWebService/src/java/br/udesc/ceavi/custovida/model/Search.java
@@ -0,0 +1,106 @@
+package br.udesc.ceavi.custodevida.model;
+
+import java.util.Date;
+
+public class Search {
+
+    private int id;
+    private Date oldDate;
+    private String oldBrand;
+    private String oldSpecification;
+    private double oldPrice;
+    private Date newDate;
+    private String newBrand;
+    private String newSpecification;
+    private double newPrice;
+    private Item item;
+    private Control control;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getOldDate() {
+        return oldDate;
+    }
+
+    public void setOldDate(Date oldDate) {
+        this.oldDate = oldDate;
+    }
+
+    public String getOldBrand() {
+        return oldBrand;
+    }
+
+    public void setOldBrand(String oldBrand) {
+        this.oldBrand = oldBrand;
+    }
+
+    public String getOldSpecification() {
+        return oldSpecification;
+    }
+
+    public void setOldSpecification(String oldSpecification) {
+        this.oldSpecification = oldSpecification;
+    }
+
+    public double getOldPrice() {
+        return oldPrice;
+    }
+
+    public void setOldPrice(double oldPrice) {
+        this.oldPrice = oldPrice;
+    }
+
+    public Date getNewDate() {
+        return newDate;
+    }
+
+    public void setNewDate(Date newDate) {
+        this.newDate = newDate;
+    }
+
+    public String getNewBrand() {
+        return newBrand;
+    }
+
+    public void setNewBrand(String newBrand) {
+        this.newBrand = newBrand;
+    }
+
+    public String getNewSpecification() {
+        return newSpecification;
+    }
+
+    public void setNewSpecification(String newSpecification) {
+        this.newSpecification = newSpecification;
+    }
+
+    public double getNewPrice() {
+        return newPrice;
+    }
+
+    public void setNewPrice(double newPrice) {
+        this.newPrice = newPrice;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public Control getControl() {
+        return control;
+    }
+
+    public void setControl(Control control) {
+        this.control = control;
+    }
+}
